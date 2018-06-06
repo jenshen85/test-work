4f454b8c99b2ab39c78883d2e6c40611afefb580
Eugene <eshereverov@gmail.com>
2018-06-06 18:54:29 +0300

    add checkbox.js

---
+++ src/js/common/checkbox.js
@@ -0,0 +1,23 @@
+"use strict"
+let arrow = document.getElementById("arrow-img");
+let inputJs = document.getElementById("skill-js__input");
+let list = document.getElementById("skills-list");
+let checkboxArr = Array.from(document.getElementsByClassName("skils-item__input"));
+var checkedArr = [];
+let allCheck = checkboxArr.length;
+
+list.addEventListener("change", () => {
+  let deg
+  checkedArr.length = 0;
+  for (let i = 0; i < checkboxArr.length; ++i) {
+    let el = checkboxArr[i];
+    if(el.checked) {       
+      checkedArr.push(el);
+    };
+  }
+  deg = (checkedArr.length / allCheck) * 100;
+  console.log(deg);
+  arrow.style.transform = `translateX(-50%) rotate(${ deg * 1.8 }deg)`;
+  inputJs.value = Math.trunc(deg);
+  return checkedArr;
+});
